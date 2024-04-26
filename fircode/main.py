1acfb374e69c5a4a8899b2a7ef6899764253e6a3
gogacoder <me@gogacoder.com>
2024-04-26 19:53:04 +0700

    Registration was added

--- fircode/main.py
+++ fircode/main.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fircode.startup import initialize_database, database_setup
-
+from fircode.models import UserRegistrationRequest
+from fircode.user_utils import create_user
+from fircode.exceptions import UserAlreadyExists
+from fastapi.exceptions import HTTPException
 
 app: FastAPI = FastAPI()
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
@@ -16,6 +19,23 @@ app.router.on_startup.append(database_setup)
 @app.get("/")
 def index_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/registration")
+async def user_registration(new_user: UserRegistrationRequest):
+    try:
+        await create_user(
+            email=new_user.email,
+            password=new_user.password,
+            first_name=new_user.first_name,
+            second_name=new_user.second_name,
+            is_admin=False
+        )
+    except UserAlreadyExists:
+        raise HTTPException(
+            status_code=409,
+            detail="User already exists"
+        )
 
 
 def start():
--- fircode/models.py
+++ fircode/models.py
@@ -1,6 +1,10 @@
 from pydantic.networks import MAX_EMAIL_LENGTH
 from tortoise import fields
 from tortoise import models
+from tortoise.contrib.pydantic import pydantic_model_creator
+from pydantic import BaseModel
+from typing_extensions import Annotated
+from pydantic import BaseModel, StringConstraints, EmailStr
 
 
 class User(models.Model):
@@ -22,10 +26,15 @@ class User(models.Model):
 class SessionToken(models.Model):
     token = fields.CharField(min_length=128, max_length=512, pk=True)
     user: fields.ForeignKeyRelation[User] = fields.ForeignKeyField(
-            "shelter.User"
+        "shelter.User"
     )
 
     class Meta:
         table = "session_token"
 
 
+class UserRegistrationRequest(BaseModel):
+    email: EmailStr
+    first_name: Annotated[str, StringConstraints(min_length=2, max_length=64)]
+    second_name: Annotated[str, StringConstraints(min_length=2, max_length=64)]
+    password: Annotated[str, StringConstraints(min_length=1, max_length=512)]
